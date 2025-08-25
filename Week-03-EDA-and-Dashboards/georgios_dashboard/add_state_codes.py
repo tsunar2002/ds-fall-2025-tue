beb268933b78918240749944b1fa4b9676a76c15
GeorgiosIoannouCoder <george_ny3@hotmail.com>
2025-08-25 05:40:38 -0400

    Week-03-Dashboard-First-Attempt

---
+++ Week-03-EDA-and-Dashboards/georgios_dashboard/add_state_codes.py
@@ -0,0 +1,36 @@
+"""
+Add state codes to geographic data for proper map functionality
+"""
+
+# Import libraries
+import pandas as pd
+
+
+# State name to state code mapping
+state_code_mapping = {
+    'Alabama': 'AL', 'Alaska': 'AK', 'Arizona': 'AZ', 'Arkansas': 'AR', 'California': 'CA',
+    'Colorado': 'CO', 'Connecticut': 'CT', 'Delaware': 'DE', 'District of Columbia': 'DC',
+    'Florida': 'FL', 'Georgia': 'GA', 'Hawaii': 'HI', 'Idaho': 'ID', 'Illinois': 'IL',
+    'Indiana': 'IN', 'Iowa': 'IA', 'Kansas': 'KS', 'Kentucky': 'KY', 'Louisiana': 'LA',
+    'Maine': 'ME', 'Maryland': 'MD', 'Massachusetts': 'MA', 'Michigan': 'MI', 'Minnesota': 'MN',
+    'Mississippi': 'MS', 'Missouri': 'MO', 'Montana': 'MT', 'Nebraska': 'NE', 'Nevada': 'NV',
+    'New Hampshire': 'NH', 'New Jersey': 'NJ', 'New Mexico': 'NM', 'New York': 'NY',
+    'North Carolina': 'NC', 'North Dakota': 'ND', 'Ohio': 'OH', 'Oklahoma': 'OK', 'Oregon': 'OR',
+    'Pennsylvania': 'PA', 'Rhode Island': 'RI', 'South Carolina': 'SC', 'South Dakota': 'SD',
+    'Tennessee': 'TN', 'Texas': 'TX', 'Utah': 'UT', 'Vermont': 'VT', 'Virginia': 'VA',
+    'Washington': 'WA', 'West Virginia': 'WV', 'Wisconsin': 'WI', 'Wyoming': 'WY'
+}
+
+# Load the geographic data
+geographic_data = pd.read_csv('processed_data/geographic_data.csv')
+
+# Add state codes column
+geographic_data['states_code'] = geographic_data['State'].map(state_code_mapping)
+
+# Save the updated data
+geographic_data.to_csv('processed_data/geographic_data.csv', index=False)
+
+print("State codes added to geographic data!")
+print(f"Updated {len(geographic_data)} records")
+print("Sample data:")
+print(geographic_data[['State', 'states_code', 'Total_Sales']].head())
